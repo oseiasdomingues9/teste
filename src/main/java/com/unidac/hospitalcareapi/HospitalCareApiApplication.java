5fcb809ad0963f5c106792c485a49bf16e7c4e31
Oséias Domingues de Oliveira <56127659+oseiasdomingues9@users.noreply.github.com>
2024-02-21 09:43:55 -0300

    Initial commit

---
+++ src/main/java/com/unidac/hospitalcareapi/HospitalCareApiApplication.java
@@ -0,0 +1,13 @@
+package com.unidac.hospitalcareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unidac/hospitalcareapi/HospitalCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unidac.hospitalcareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
